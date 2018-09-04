25ac27c9f81064891b3dc1f43765fd6fb16f1c9c
tuhbm <tuhbm2@gmail.com>
2018-09-04 15:36:21 +0900

    propTypes 추가

--- my-todo-list/src/App.js
+++ my-todo-list/src/App.js
@@ -6,11 +6,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <br/>
+        <br/>
         <MyComponent /* 디폴트 프롭스 사용 */ />
         <br/>
         <br/>
         <br/>
-        <MyComponent name="props" text="react props" />
+        <MyComponent name={1} text={2} />
       </div>
     );
   }
--- my-todo-list/src/MyComponent.js
+++ my-todo-list/src/MyComponent.js
@@ -1,20 +1,32 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class MyComponent extends Component {
+    static defaultProps = {
+        name: 'name props default',
+        text: 'text props default'
+    }
+
+    static propTypes = {
+        name: PropTypes.string,
+        text: PropTypes.string,
+        age: PropTypes.number.isRequired
+    }
+
     render () {
         return (
             <div>
                 <div>My Component 불러오기</div>
+                <br/>
+                <br/>
+                <h2>Props 사용하기</h2>
                 <div>{ this.props.name } 사용하기</div>
                 <div>{ this.props.text } 추가</div>
+                <div>{ this.props.age }살 입니다.</div>
             </div>
         )
     }
 }
 
-MyComponent.defaultProps = {
-    name: 'name',
-    text: 'text'
-}
 
 export default MyComponent;
