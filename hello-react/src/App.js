4ecf139e679a402a29ee7fa478d8117b0c36febc
tuhbm <tuhbm2@gmail.com>
2018-09-04 11:41:26 +0900

    [2강]주석및 돔 변경해보기

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,18 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+    const text = '당신은 어썸한가요ㅋㅋㅋ?';
+    const condition = true;
+    const condition2 = false;
+    const style = {
+      backgroundColor: 'pink',
+      border: '1px solid black',
+      height: Math.round(Math.random() * 300) + 50,
+      width: Math.round(Math.random() * 300) + 50,
+      WebkitTransition: 'all',
+      MozTransition: 'all',
+      msTransition: 'all'
+    }
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="my-div">
+        <h1 className="helloReact">리액트 안녕!</h1>
+        <h2>{text}</h2>
+        <form>
+          <fieldset>필드셋</fieldset>
+          <input type="text" placeholder="아이디" /><br/>
+          <input type="password" placeholder="비밀번호" /><br/>
+        </form>
+        {/* 주석은 요로코롬 */}
+        //주석을 이렇게 달면 걍 노출된다.
+        <br/>
+        /* 이것도 그냥 노출된다......... */
+        <div /* 이건 허용된다....*/>주석용 돔입니다.</div>
+        {
+          condition ? '참' : '거짓'
+        }
+        <br/>
+        {
+          condition && '참'
+        }
+        <br/>
+        {
+          condition2 || '거짓'
+        }
+        <div style={style}></div>
       </div>
     );
   }
