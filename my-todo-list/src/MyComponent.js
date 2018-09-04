4081b8dbb890046270aaf55fd15fe9af1b4b2970
tuhbm <tuhbm2@gmail.com>
2018-09-04 13:52:37 +0900

    react import & export 연습

---
+++ my-todo-list/src/MyComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class myComponent extends Component {
+    render () {
+        return (
+            <div>
+                나의 새로운 컴포넌트!!!!!!!!!!!!
+            </div>
+        )
+    }
+}
+
+export default myComponent;
