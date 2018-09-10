9514218b33d350f178e5966639616fc36ecd2014
TaekyunKim <tuhbm2@gmail.com>
2018-09-10 22:57:46 +0900

    file name modify

---
+++ iterate_components/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import IterationSimple from './IterationSample';
+
+class App extends Component {
+  render() {
+    return (
+      <IterationSample/>
+    );
+  }
+}
+
+export default App;
--- iterate_components/src/IterationSample.js
+++ iterate_components/src/IterationSample.js
@@ -2,12 +2,11 @@ import React, {Component} from 'react';
 
 class IterationSample extends Component {
   render() {
+    const names = ['눈사람','얼음','눈','바람'];
+    const nameList = names.map(name => `<li>{name}</li>`);
     return (
       <ul>
-        <li>눈사람</li>
-        <li>얼음</li>
-        <li>눈</li>
-        <li>바람</li>
+        {nameList}
       </ul>
     );
   }
