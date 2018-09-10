ef835a00e7a292a4f037e5da69c0978057b1eed9
TaekyunKim <tuhbm2@gmail.com>
2018-09-10 23:31:05 +0900

    removeItem methods add

--- iterate_components/src/IterationSample.js
+++ iterate_components/src/IterationSample.js
@@ -1,13 +1,62 @@
 import React, {Component} from 'react';
 
 class IterationSample extends Component {
+  state = {
+    names : ['눈사람','얼음','눈','바람'],
+    name: ''
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      name: e.target.value
+    })
+  };
+
+  handleInsert = () => {
+    this.setState({
+      names: this.state.names.concat(this.state.name)
+    });
+  };
+
+  handleRemove = (index) => {
+    const { names } = this.state;
+    this.setState({
+      names: [
+        ...names.slice(0, index),
+        ...names.slice(index + 1, names.length)
+      ]
+    });
+  }
+
+  handleKeypress = (e) => {
+    if(e.key === 'Enter') {
+      this.handleInsert();
+      this.state.name = '';
+    }
+  };
+
   render() {
-    const names = ['눈사람','얼음','눈','바람'];
-    const nameList = names.map((name,index) => <li key={index}>{name}</li>);
+    const nameList = this.state.names.map((name,index) =>
+      <li
+          key={index}
+          onDoubleClick={() => this.handleRemove(index)}>
+      {name}
+      </li>
+    );
+
     return (
-      <ul>
-        {nameList}
-      </ul>
+      <div>
+        <input
+          type="text"
+          onChange={this.handleChange}
+          value={this.state.name}
+          onKeyPress={this.handleKeypress}
+        />
+        <button onClick={this.handleInsert}>추가</button>
+        <ul>
+          {nameList}
+        </ul>
+      </div>
     );
   }
 }
