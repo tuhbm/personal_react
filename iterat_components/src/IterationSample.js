0340e3ddc67ed237294ebe5021f8c64e12b9f4d5
TaekyunKim <tuhbm2@gmail.com>
2018-09-10 22:45:48 +0900

    iterationComponents start

---
+++ iterat_components/src/IterationSample.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+class IterationSample extends Component {
+  render() {
+    return (
+      <ul>
+        <li>눈사람</li>
+        <li>얼음</li>
+        <li>눈</li>
+        <li>바람</li>
+      </ul>
+    );
+  }
+}
+
+export default IterationSample;
