10aafedc06f9215d883d2eaf157061e30684b9f9
TaekyunKim <tuhbm2@gmail.com>
2018-09-10 23:10:48 +0900

    react iterationSample components first commit

--- iterate_components/src/IterationSample.js
+++ iterate_components/src/IterationSample.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 class IterationSample extends Component {
   render() {
     const names = ['눈사람','얼음','눈','바람'];
-    const nameList = names.map(name => `<li>{name}</li>`);
+    const nameList = names.map((name,index) => <li key={index}>{name}</li>);
     return (
       <ul>
         {nameList}
