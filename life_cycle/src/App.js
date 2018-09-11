a14891b40d706788d0a73cb4c481431fdb9f7a42
TaekyunKim <tuhbm2@gmail.com>
2018-09-11 22:49:24 +0900

    life cycle sample folder add

---
+++ life_cycle/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import LifeCycleSample from './LifeCycleSample'
+
+class App extends Component {
+  render() {
+    return (
+      <LifeCycleSample/>
+    );
+  }
+}
+
+export default App;
---
+++ life_cycle/src/LifeCycleSample.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class LifeCycleSample extends Component {
+  render() {
+    return (
+      <div>
+        LifeCycleSample
+      </div>
+    );
+  }
+}
+
+export default LifeCycleSample;
