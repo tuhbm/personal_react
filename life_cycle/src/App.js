358481f03d002b4245fb413584ab5aaad345d0eb
tuhbm <tuhbm2@gmail.com>
2018-09-18 16:52:00 +0900

    lifeCycleSample update

--- life_cycle/src/App.js
+++ life_cycle/src/App.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
 import LifeCycleSample from './LifeCycleSample'
 
+const getRandomColor = () => {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+};
+
 class App extends Component {
+  state = {
+    color: '#000000'
+  };
+  
+  handleClick = () => {
+    this.setState({
+        color: getRandomColor()
+    });
+  };
+  
   render() {
     return (
-      <LifeCycleSample/>
+        <div>
+          <button onClick={this.handleClick}>랜덤 색상</button>
+          <LifeCycleSample color={this.state.color}/>
+        </div>
     );
   }
 }
