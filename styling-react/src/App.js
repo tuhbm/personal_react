c52fe43bf70b5ee10ed410f98bcef09b795f9d82
tuhbm <tuhbm2@gmail.com>
2018-09-28 17:57:00 +0900

    styling start

---
+++ styling-react/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import styles from './App.css';
+
+console.log(styles);
+
+class App extends Component {
+  render() {
+    return (
+      <div className={styles.box}>
+      
+      </div>
+    );
+  }
+}
+
+export default App;
